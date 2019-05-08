e36ce122e8922243de81473a3aeb9b888c478507
Gabriel Varadi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-05-08 17:31:52 +0000

    done a bunch of stuff without commiting woops basically implemented cookies, login and logout

--- express.server.js
+++ express.server.js
@@ -45,7 +45,8 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let templateVars = {
-    urls: urlDatabase
+    urls: urlDatabase,
+    username: req.cookies["username"]
   }
   res.render("urls_index", templateVars);
 });
@@ -60,13 +61,22 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${rand_url}`);
 });
 
+app.post('/logout', (req, res) => {
+const cooki = req.cookies["username"]
+res.clearCookie('username')
+res.status(302).redirect('/urls')
+})
 
 app.post("/urls/:shortURL", (req, res) => { //what does it do?
-  console.log(req.body)
   const shortURL = req.params.shortURL
   const longURL = req.body.longURL
   updateURL(shortURL, longURL)
   res.status(302).redirect('/urls')
+})
+
+app.post('/login', (req, res) => {
+res.cookie('username', req.body.username)
+res.status(302).redirect('/urls')
 })
 
 
@@ -77,7 +87,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -88,7 +102,8 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
